b4f62267dcac2803b075f3f1dd0a6717c64360cb
bkmaz <kundaimazintech@gmail.com>
2021-04-12 21:15:10 +0200

    deviare initial commit

---
+++ src/weatherapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+import requests
+
+# Create your views here.
+
+
+def get_weather(request):
+    api_key = "3de49393e4baaf1d1a4a326034002890"
+    city_id = "993800"
+    url = "http://api.openweathermap.org/data/2.5/forecast"
+    querystring = {"id": 993800, "appid": api_key }
+    responses = requests.request("GET", url, params=querystring)
+    from django.http import JsonResponse, response, HttpResponse
+    # return JsonResponse(responses.content, safe=False)
+
+    return HttpResponse(responses.content)
